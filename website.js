6d44c707f88bbe9f4f0cfe51cb2fe352f03a61ca
BBhavya25 <bhavyabukya25@gmail.com>
2024-08-25 21:21:20 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById('dropdown');
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'block';
+    }
+}
+
+function toggleMainDropdown() {
+    var dropdown = document.getElementById('main-dropdown');
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'block';
+    }
+}
